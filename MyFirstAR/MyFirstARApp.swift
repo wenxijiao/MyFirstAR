60244ed7d1bf1a9af119855ca56efd8c7698b3e4
wenxijiao <vincentjiao.dream@gmail.com>
2025-12-13 11:27:04 +1300

    Initial Commit

---
+++ MyFirstAR/MyFirstARApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstARApp.swift
+//  MyFirstAR
+//
+//  Created by Vincent Jiao on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
